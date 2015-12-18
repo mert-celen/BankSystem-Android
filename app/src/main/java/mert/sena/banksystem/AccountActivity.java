f5c6d8f2e1fb5aef2c00ba953e05de40c9a6a60e
mert-celen <mcelen94@gmail.com>
2015-12-18 22:22:20 +0200

    Cleanup, unnecessary class delete

--- app/src/main/java/mert/sena/banksystem/AccountActivity.java
+++ app/src/main/java/mert/sena/banksystem/AccountActivity.java
@@ -1,11 +1,16 @@
 package mert.sena.banksystem;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,8 +23,9 @@ public class AccountActivity extends AppCompatActivity {
         while(app.accounts.getObj(app.currentid).hasmoreStack()){
             displayMessage(app.accounts.getObj(app.currentid).getfromStack());
         }
-        this.init();
 
+        displayMessage("Welcome " + app.accounts.getObj(app.currentid).getName());
+        this.init();
     }
 
     public void init(){
@@ -42,8 +48,7 @@ public class AccountActivity extends AppCompatActivity {
         withdrawButton.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                        Intent intent1 = new Intent(AccountActivity.this, WithdrawMoneyActivity.class);
-                        startActivity(intent1);
+withdrawMoney();
                     }
                 }
         );
@@ -51,7 +56,7 @@ public class AccountActivity extends AppCompatActivity {
         payDeptButton.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-
+                        payDept();
                     }
                 }
         );
@@ -80,5 +85,68 @@ public class AccountActivity extends AppCompatActivity {
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        init();
+    }
+
+    private void payDept(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter Amount");
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_NUMBER_VARIATION_NORMAL);
+        builder.setView(input);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                boolean flag = app.accounts.payDept(app.currentid, Integer.parseInt(input.getText().toString()));
+                TextView statusLabel = (TextView)findViewById(R.id.statusLabel);
+                if (flag) {
+                    statusLabel.setText("Status : "+ Integer.parseInt(input.getText().toString()) + " dept paid.");
+                    init();
+                } else {
+                    statusLabel.setText("Status : Not enough money!");
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
+
+    private void withdrawMoney(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter Amount");
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_NUMBER_VARIATION_NORMAL);
+        builder.setView(input);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                boolean flag = app.accounts.getObj(app.currentid).removeMoney(Double.parseDouble
+                        (input.getText().toString()));
+                TextView statusLabel = (TextView)findViewById(R.id.statusLabel);
+                if (flag) {
+                    statusLabel.setText("Status : "+ Integer.parseInt(input.getText().toString()) + " money withdrawn.");
+                    init();
+                } else {
+                    statusLabel.setText("Status : Not enough money!");
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
     }
 }
